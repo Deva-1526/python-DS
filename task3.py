bd3bd0a2bab5acbc5e2bea9b7b038c0eae582700
Deva-1526 <rdevadharshini555@gmail.com>
2026-05-02 19:23:26 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,10 @@
-'''#Smart Attendance System
+#Smart Attendance System
 def Smart_Attendance_System(student):
     for name,time in student:
         if time <= 9.05 :
             print("Present:",name)      
 student=[("adhi",9.00),("deva",9.02),("kalai",9.10),("sham",9.08)]
-Smart_Attendance_System(student)'''
+Smart_Attendance_System(student)
 
 #E-commerce Discount Engine
 def Ecommerce_Discount_Engine():
